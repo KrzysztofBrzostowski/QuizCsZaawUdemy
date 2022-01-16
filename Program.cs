68f4aacb55aeb3cb7640d98075eadc7769eecad7
Christopher Brzostowski <kbrzostowski@gmail.com>
2022-01-16 20:03:48 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizCsZaawUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quiz = new Quiz();
+            quiz.Start();
+        }
+    }
+}
